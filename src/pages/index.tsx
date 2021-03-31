f0fe8b43c59b6b5577c3522bdecaff7c8224c0e3
Pedro César <pedrocs378@gmail.com>
2021-03-31 17:27:23 -0300

    feat: cria pagina de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -7,7 +10,26 @@ export default function Home() {
         <title>dashgo</title>
       </Head>
 
-      <div>TESTE</div>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDir="column"
+        >
+          <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Password" />
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
     </>
   )
 }
