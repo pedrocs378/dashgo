c75d92dc06f7d9aa5e341412f9e058fce3cd4a57
Pedro César <pedrocs378@gmail.com>
2021-03-31 16:55:02 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>dashgo</title>
+      </Head>
+
+      <div>TESTE</div>
+    </>
+  )
+}
