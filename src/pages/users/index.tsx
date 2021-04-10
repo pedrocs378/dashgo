dedf707e617e7d205a306cb4d2b9c5adc42235b5
Pedro César <pedrocs378@gmail.com>
2021-04-10 19:32:31 -0300

    chore: separa a listagem de usuario em um hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,33 +1,16 @@
 import { useEffect } from "react";
-import { useQuery } from 'react-query'
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Spinner, Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from 'next/link'
+
+import { useUsers } from "../../services/hooks/useUsers";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-	const { data, isLoading, error } = useQuery('users', async () => {
-		const response = await fetch('http://localhost:3000/api/users')
-		const data = await response.json()
-
-		const users = data.users.map(user => {
-			return {
-				id: user.id,
-				name: user.name,
-				email: user.email,
-				createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-					day: '2-digit',
-					month: 'long',
-					year: 'numeric'
-				})
-			}
-		})
-
-		return users
-	})
+	const { data, isLoading, isFetching, error } = useUsers()
 
 	const isWideVersion = useBreakpointValue({
 		base: false,
@@ -49,6 +32,8 @@ export default function UserList() {
 					<Flex mb="8" justify="space-between" align="center">
 						<Heading size="lg" fontWeight="normal">
 							Usuários
+
+							{!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
 						</Heading>
 
 						<Link href="/users/create" passHref>
